8c924922a413044361507b7833e680134cb6bf74
Hussein Najah <husseinnajah123@gmail.com>
2022-07-22 00:19:02 +0300

    10

--- src/components/AppLink.vue
+++ src/components/AppLink.vue
@@ -1,22 +1,26 @@
 <template>
-  <a 
-    v-if="isExternal" 
+  <a
+    v-if="isExternal"
     target="_blank"
     rel="noopener"
     class="external-link"
-    :href="to"><slot/></a>
-  <router-link v-else v-bind="$props" class="internal-link"><slot/></router-link>
+    :href="to"
+    ><slot
+  /></a>
+  <router-link v-else v-bind="$props" class="internal-link"
+    ><slot
+  /></router-link>
 </template>
 <script>
-import {RouterLink} from 'vue-router'
+import { RouterLink } from "vue-router";
 export default {
-  props:{
-    ...RouterLink.props
+  props: {
+    ...RouterLink.props,
   },
-  computed:{
-    isExternal(){
-      return typeof this.to === 'string' && this.to.startsWith('http')
-    }
-  }
-}
+  computed: {
+    isExternal() {
+      return typeof this.to === "string" && this.to.startsWith("http");
+    },
+  },
+};
 </script>
